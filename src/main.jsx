8e93b645dcac37a3f85662d83f478c04581396cf
Alejandrocsdev <alejandrocsdev@gmail.com>
2024-04-23 13:48:38 +0800

    practice 20

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './18/App.jsx'
+import App from './20/App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
